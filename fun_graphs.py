9c61105825eaf96bb38398081ffe415cb75c34bb
Joshua David Frank <146133626+17frankj@users.noreply.github.com>
2025-01-23 17:42:56 -0500

    Update fun_graphs.py

--- fun_graphs.py
+++ fun_graphs.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# Sample DataFrame
+df = pd.DataFrame({
+    'A': [1, 2, 3],
+    'B': [4, 5, 6]
+})
+
+# Save DataFrame as an image
+df.to_string(buf=open('output.txt', 'w'))
+
+# Save Plot as an image
+df.plot(kind='bar')
+plt.savefig('output.png')
